1ef95a2c263d54689e951e22d15b68b738b7fed1
ZuzaNeto <zuzanetobra@gmail.com>
2026-03-01 18:19:47 -0300

    inicializando a estrutura do projeto e importações dos módulos

---
+++ interface.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+# Importando os modulos desenvolvidos por Emanuel e Josuel
+from base_lexica import AnalisadorLexicoAFD
+from sintaxe import AnalisadorSintatico
+from modulo_matematico import ModuloMatematico
+
